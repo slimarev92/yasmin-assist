2125b2d7d9382c889267a4f9ed2d2a2eefa685ba
Sasha Limarev <slimarev@outlook.com>
2023-06-16 17:22:28 +0300

    Fixed typo.

--- google-custom.ts
+++ google-custom.ts
@@ -1,6 +1,6 @@
 import "./google-custom-style.css";
 
-const ENGINGE_IDS = ["b2c8e585118724176", "83f35701b6a984777", "d27d3a215df6b46ae"];
+const ENGINE_IDS = ["b2c8e585118724176", "83f35701b6a984777", "d27d3a215df6b46ae"];
 
 function getRandom(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -20,7 +20,7 @@ function initCommunication() {
 
 const scriptElement = document.createElement("script");
 
-scriptElement.src = `https://cse.google.com/cse.js?cx=${ENGINGE_IDS[getRandom(0, 3)]}`;
+scriptElement.src = `https://cse.google.com/cse.js?cx=${ENGINE_IDS[getRandom(0, 3)]}`;
 scriptElement.async = true;
 
 document.body.appendChild(scriptElement);
